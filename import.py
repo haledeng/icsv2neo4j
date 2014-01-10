184bc41fca4b89b4f7ed9ad7ca723bc2f88c435f
hale_deng <dhlwust@163.com>
2014-01-10 10:42:13 +0800

    Create import.py

---
+++ import.py
@@ -0,0 +1,30 @@
+# import triple into neo4j from csv file downloaded from dbpedia
+import csv
+from py2neo import neo4j, node, rel, cypher
+graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+
+ifile = open("College.csv","rb")
+reader = csv.reader(ifile)
+graph_db.clear()
+
+reader = list(reader)
+relations = reader[0]
+# row 2 is full uri of row 1. row 2 = namespace + row 1
+properties = reader[2]
+
+rownum = 5
+for row in reader[4:]:
+    subject, = graph_db.create(
+        {properties[1]:row[1]}
+    )
+    # first one is subject
+    objects = row[1:]
+    colnum = 2
+    for col in objects:
+        if col != "NULL":
+            object, rel = graph_db.create(
+                {properties[colnum-1]:col},(subject, relations[colnum-1],0)
+            )
+        colnum = colnum + 1
+    rownum = rownum + 1
+ifile.close()
